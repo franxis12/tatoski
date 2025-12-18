c1a3618726cc1e3be5a1c6f2f142162ca650c1f6
Francis Martinez <f.martinez1993@icloud.com>
2025-12-17 19:42:04 -0500

    test

--- src/components/AddApplication.jsx
+++ src/components/AddApplication.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Input from "../components/Input";
 
 function AddApplication() {
+  //
   return (
     <div className="bg-black/80 backdrop-blur-sm w-screen h-screen absolute flex items-center justify-center">
       <div className="min-w-5/8 min-h-5/8 bg-(--CardBg) rounded-2xl border border-slate-50/50 shadow-2xl p-5">
