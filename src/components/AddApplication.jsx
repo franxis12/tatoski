dfd02dea0c145775a2e1567e48e0fc3625613c96
Francis Martinez <f.martinez1993@icloud.com>
2025-12-17 19:40:49 -0500

    Inputs on form complete

--- src/components/AddApplication.jsx
+++ src/components/AddApplication.jsx
@@ -5,47 +5,52 @@ function AddApplication() {
   return (
     <div className="bg-black/80 backdrop-blur-sm w-screen h-screen absolute flex items-center justify-center">
       <div className="min-w-5/8 min-h-5/8 bg-(--CardBg) rounded-2xl border border-slate-50/50 shadow-2xl p-5">
-        <div className="flex w-full items-end gap-2">
+        <div className="flex w-full items-end gap-2 mb-2">
           <div className="flex-col flex w-full">
             <label className="text-xl font-medium">Job Title</label>
             <input
               placeholder="Job title"
-              className="bg-white rounded-md h-8 p-2"
+              className="bg-white rounded-md h-8 p-3"
             ></input>
           </div>
           {/*Where did applied*/}
           <div></div>
           <input
             type="date"
-            className="bg-white rounded-md h-8 p-2 border "
+            className="bg-white rounded-md h-8 p-3 border "
           ></input>
         </div>
 
-        <div className="flex w-full items-end gap-2">
+        <div className="flex w-full items-end gap-2 mb-3">
           <div className="flex-col flex w-full">
-            <label className="text-xl font-medium">Job Title</label>
+            <label className="text-xl font-medium">Salary</label>
             <input
               placeholder="Job title"
-              className="bg-white rounded-md h-8 p-2"
+              className="bg-white rounded-md h-8 p-3"
             ></input>
           </div>
           <div className="flex-col flex w-full">
-            <label className="text-xl font-medium">Job Title</label>
+            <label className="text-xl font-medium">Type</label>
             <input
               placeholder="Job title"
-              className="bg-white rounded-md h-8 p-2"
+              className="bg-white rounded-md h-8 p-3"
             ></input>
           </div>
+
           <div className="flex-col flex w-full">
-            <label className="text-xl font-medium">Job Title</label>
+            <label className="text-xl font-medium">Resume</label>
             <input
               placeholder="Job title"
-              className="bg-white rounded-md h-8 p-2"
+              className="bg-white rounded-md h-8 p-3"
             ></input>
           </div>
         </div>
-        <div>
-          <textarea className="bg-white"></textarea>
+        <div className="">
+          <label className="text-xl font-medium">Job description</label>
+          <textarea
+            placeholder="Paste here your job description"
+            className="bg-white w-full m-h-full rounded-xl  h-90 p-3"
+          ></textarea>
         </div>
       </div>
     </div>
