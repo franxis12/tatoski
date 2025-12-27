69c87253bd04d2c4e4aaf00281b3f7f0cb7fda31
Francis Martinez <f.martinez1993@icloud.com>
2025-12-26 19:13:48 -0500

    Counter Style update

--- src/pages/Counter.jsx
+++ src/pages/Counter.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-const TARGET_DATE = new Date("2026-01-01T18:00:00");
+const TARGET_DATE = new Date("2026-01-01T19:30:00");
 
 const getTimeLeft = () => {
   const diff = TARGET_DATE.getTime() - Date.now();
@@ -15,7 +15,7 @@ const getTimeLeft = () => {
 
 function Counter() {
   const [timeLeft, setTimeLeft] = useState(getTimeLeft());
-  const [name, setName] = useState("Jas");
+  const [name, setName] = useState("Fran");
 
   const handleChangeName = () => {
     if (name === "Jas") {
@@ -31,17 +31,26 @@ function Counter() {
   }, []);
 
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-(--PrimaryColor) text-white">
-      <div>
+    <div className="flex h-screen w-screen items-center justify-center bg-red-400 text-white">
+      <div className="flex items-center justify-center flex-col border-2 p-2 rounded-xl bg-black/10 shadow-2xl">
         <h1
-          className="mb-6 text-3xl font-semibold cursor-pointer"
+          className="mb-6 text-2xl font-medium cursor-pointer"
           onClick={handleChangeName}
         >
-          Time remaining to see {name} ❤️
+          Time remaining to see
         </h1>
+        <h2
+          onClick={handleChangeName}
+          className="mb-6 text-4xl font-semibold cursor-pointer animate-bounce"
+        >
+          ❤️ {name} ❤️
+        </h2>
         <div className="grid grid-cols-4 gap-4 text-2xl">
           {Object.entries(timeLeft).map(([label, value]) => (
-            <div key={label} className="rounded-xl bg-white/10 p-4">
+            <div
+              key={label}
+              className="rounded-xl bg-white/10 p-4 flex flex-col items-center"
+            >
               <div className="text-4xl font-bold">
                 {value.toString().padStart(2, "0")}
               </div>
