9ca8c2fe9bb56f731303e7ea50ffcb22ecb40b84
Francis Martinez <f.martinez1993@icloud.com>
2026-01-01 01:13:50 -0500

    comments

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,10 +2,11 @@ import React from "react";
 
 function Button({ children, onClick }) {
   return (
+    //This button needs more features like icons and type of button
     <div>
       <button
         onClick={onClick}
-        className="bg-(--BtnPrimary) hover:bg-(--BtnPrimary_Hover) rounded-lg px-5 py-2 transition-all duration-300 font-medium"
+        className="bg-(--BtnPrimary)  hover:bg-(--BtnPrimary_Hover) rounded-lg px-5 py-2 transition-all duration-300 font-medium"
       >
         {children}
       </button>
